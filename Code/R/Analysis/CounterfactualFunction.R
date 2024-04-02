b56e67d77d8a573d8ff9206e1db535bb560bfb17
Sandeep Sharma <imsndp10@gmail.com>
2024-04-02 19:41:53 +0545

    models added

--- Code/R/Analysis/CounterfactualFunction.R
+++ Code/R/Analysis/CounterfactualFunction.R
@@ -118,4 +118,30 @@ CounterFac <- function(Year, Gender = c("all", "male", "female"),
 a <- CounterFac(Year = 2018,Gender = "all", region = "all",industry = "all",formType = "HH",
                 reg = 2)
 
+b <- CounterFac(Year = 2018,Gender = "all", region = "all",industry = "all",formType = "JM_NoInd",
+                reg = 2)
+
+c <- CounterFac(Year = 2018,Gender = "all", region = "all", industry = "man" ,formType = "JM_NoInd",
+                reg = 2)
+
+d <- CounterFac(Year = 2018,Gender = "all", region = "all", industry = "ser" ,formType = "JM_NoInd",
+                reg = 2)
+
+e <- CounterFac(Year = 2018,Gender = "all", region = "all", industry = "man_ser" ,formType = "JM_NoInd",
+                reg = 2)
+
+f <- CounterFac(Year = 2008,Gender = "all", region = "all",industry = "all",formType = "JM_NoInd",
+                reg = 2)
+
+g <- CounterFac(Year = 2008,Gender = "all", region = "all", industry = "man" ,formType = "JM_NoInd",
+                reg = 2)
+
+h <- CounterFac(Year = 2008,Gender = "all", region = "all", industry = "ser" ,formType = "JM_NoInd",
+                reg = 2)
+
+i <- CounterFac(Year = 2008,Gender = "all", region = "all", industry = "man_ser" ,formType = "JM_NoInd",
+                reg = 2)
+
+
+
 
