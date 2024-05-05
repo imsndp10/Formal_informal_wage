a0a19d933d8f18ad0624e6f2292444733ba4dabb
Sandeep Sharma <imsndp10@gmail.com>
2024-05-05 12:35:03 +0545

    commit new

--- Code/R/Analysis/Formal_estimation.R
+++ Code/R/Analysis/Formal_estimation.R
@@ -24,9 +24,6 @@ dataFunc <- function(Year, industry = NULL, job = NULL){
   }
   return(Data)
   }
-a <- dataFunc(Year = 2008,industry = c("Market_services", "Non_Market_services",
-                                       "Arts_entertain"),
-              job = c("Managers")) 
 
 estimationFunc <- function(Year, industry = NULL, job = NULL){
   Data <- dataFunc(Year,industry,job)
@@ -39,11 +36,12 @@ estimationFunc <- function(Year, industry = NULL, job = NULL){
 tableOutput <- function(Year, industry = NULL, job = NULL){
   reg1 <- estimationFunc(Year = 2008, industry, job)
   reg2 <- estimationFunc(Year = 2018, industry, job)
-  etable(list(reg1,reg2), title = "Formal informal determination",
+  etable(list(reg1,reg2), title = paste0("Formal informal","_", job[1]),
          dict = c(HH_formal = "Formal members", dep_ratio = "Dependent",
                   yrs_schooling = "Years of schooling", female = "Female",
                   caste_group_6 = "Caste", dist = "District"),
          tex = TRUE,
+         adjustbox = TRUE,
          file = paste0("../../../Output/Tables/formal_informal",job[1],".tex"))
 }
 
@@ -59,34 +57,3 @@ tableOutput(industry = c("Market_services", "Non_Market_services",
                          "Arts_entertain"),
             job = c("Elementary_occupations", "Plant_operator",
                     "Agri_trade"))
-
-etable(estimationFunc(Year = 2008,industry = c("Market_services", "Non_Market_services",
-                                               "Arts_entertain"),
-                      job = c("Managers") ))
-fixest::etable()
-
-data <- readRDS("../../../Data/Cleaned/Pooled/Pooled.RDS") %>% 
-  group_by(psu, hhid, year) %>% 
-  mutate(HH_formal = if_else(formal_employment == 1, sum(formal_employment) - 1,
-                             sum(formal_employment))) %>% 
-  ungroup() %>% 
-  filter(year == 2008)
-
-data1 <- readRDS("../../../Data/Cleaned/Pooled/Pooled.RDS") %>% 
-  group_by(psu, hhid, year) %>% 
-  mutate(HH_formal = if_else(formal_employment == 1, sum(formal_employment) - 1,
-                             sum(formal_employment))) %>% 
-  ungroup() %>% 
-  filter(year == 2018)
-
-bdat <- fixest::feols(formal_employment ~ HH_formal + dep_ratio + female + urban +
-                        experience + experience_sq | yrs_schooling ~ average factor(dist), data = data)  
-
-etable(bdat)
-adat <- fixest::feols(formal_employment ~ HH_formal + dep_ratio +  
-                experience + experience_sq + yrs_schooling| csw0(factor(caste_group_6), 
-                                                                 female, urban,
-                                                                 factor(dist)) , data = data)  
-
-etable(adat)
-etable(adat, tex = TRUE, file = "../../../Final Paper/final_paper/tables/endogenity.tex")
---
+++ Code/R/Analysis/densityPlot.R
@@ -0,0 +1,61 @@
+if(!is.null(dev.list())) dev.off()
+rm(list=ls())
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+cat("\014") 
+
+library("tidyverse")
+library("ggplot2")
+
+
+dataFunc <- function(Year, industry = NULL, job = NULL){
+  data <- readRDS("../../../Data/Cleaned/Pooled/Pooled.RDS") %>%
+    group_by(psu, hhid, year) %>% 
+    mutate(HH_formal = if_else(formal_employment == 1, sum(formal_employment) - 1,
+                               sum(formal_employment))) %>% 
+    ungroup() %>% 
+    filter(year == Year)
+  if(is.null(industry) & is.null(job)){
+    Data <- data
+  }else if(!is.null(industry) & !is.null(job)){
+    Data <- data %>% 
+      filter(job_sector%in%industry &
+               class_5%in%job)
+  }
+  Data <- Data %>% 
+    group_by(formal_employment) %>% 
+    mutate(mean_schooling = mean(yrs_schooling)) %>% 
+    ungroup()
+  return(Data)
+}
+
+
+
+plotFun <- function(Year, industry= NULL, job=NULL){
+  ddat <- dataFunc(Year, industry, job)
+  plot <- ggplot(data = ddat)+
+    geom_density(aes(x = yrs_schooling, fill = factor(formal_employment)),stat = "density", 
+                 position = "identity", alpha = 0.3 )+
+    geom_vline(aes(xintercept = mean_schooling, color = factor(formal_employment)), linetype = "longdash",
+               show.legend = FALSE )+
+    theme_bw()+
+    theme(legend.position = "top",panel.grid.minor = element_blank())+
+    labs(x = "Years of schooling",
+         y = "Density",
+         fill = "Employment")+
+    scale_color_brewer(type = "seq",  palette ="Set1" )+
+    #scale_fill_brewer(type = "seq", palette = "Set1")+
+    scale_fill_discrete(labels = c("Informal", "Formal"))  
+  return(plot)
+}
+
+Plot1 <- plotFun(Year = 2008, industry = c("Market_services", "Non_Market_services",
+                                        "Arts_entertain"),
+              job = c("Managers"))
+Plot2 <- plotFun(Year = 2008, industry = c("Market_services", "Non_Market_services",
+                                           "Arts_entertain"),
+                 job = c("Clerical_sales"))
+Plot3 <- plotFun(Year = 2008, industry = c("Market_services", "Non_Market_services",
+                                           "Arts_entertain"),
+                 job = c("Elementary_occupations", "Plant_operator",
+                         "Agri_trade"))
+
